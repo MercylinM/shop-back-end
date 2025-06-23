414bd7b3b08e57ac4549b9960ff9b8ea91c021c9
mercylin muthoni <muthonimercylin@gmail.com>
2025-06-23 16:50:22 +0300

    add api module

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=28)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=28)
+
 class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(max_digits = 10, decimal_places = 2)
     stock = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, null=True, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'{self.name} || price {self.price}' 
+    
+class Subscription(models.Model):
+    name = models.CharField(max_length=50)
+
+class Customer(models.Model):
+    subscription = models.OneToOneField(Subscription, on_delete=models.CASCADE)
